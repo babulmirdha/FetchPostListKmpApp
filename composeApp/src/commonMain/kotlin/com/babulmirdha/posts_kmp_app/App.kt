4391b6eb4f8f3cacdfad26ed01c631b4498f1929
Babul Mirdha <babumirdha@gmail.com>
2025-07-15 15:00:16 +0600

    add users list

--- composeApp/src/commonMain/kotlin/com/babulmirdha/posts_kmp_app/App.kt
+++ composeApp/src/commonMain/kotlin/com/babulmirdha/posts_kmp_app/App.kt
@@ -2,8 +2,11 @@ package com.babulmirdha.posts_kmp_app
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
+import com.babulmirdha.posts_kmp_app.screens.UserListScreen
 import com.example.post_list_app.providers.HttpClientEngineProvider
 import com.babulmirdha.posts_kmp_app.services.PostService
+import com.babulmirdha.posts_kmp_app.services.UserService
+import com.babulmirdha.posts_kmp_app.viewmodels.UserViewModel
 import com.example.post_list_app.screens.PostListScreen
 import com.example.post_list_app.viewmodels.PostViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -12,7 +15,10 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
-        val viewModel = remember { PostViewModel(PostService(HttpClientEngineProvider.createHttpClient())) }
-        PostListScreen(viewModel)
+        val userViewModel = remember { UserViewModel(UserService(HttpClientEngineProvider.createHttpClient())) }
+        UserListScreen(userViewModel)
+
+//        val postViewModel = remember { PostViewModel(PostService(HttpClientEngineProvider.createHttpClient())) }
+//        PostListScreen(postViewModel)
     }
 }
